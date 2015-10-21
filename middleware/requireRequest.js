3ec579783cb04b02b19a0bc41be441cc04594ce0
Dawid <dcz31249@gmail.com>
2015-10-21 12:49:47 +0200

    better request validion for /auth

--- middleware/requireRequest.js
+++ middleware/requireRequest.js
@@ -10,6 +10,7 @@ var _ = require('lodash');
 /**
  * passes only requests that have given fields specified in the fieldsRequired param. 
  * If it passes it will created an parsedRequest Object on the context for convinence.
+ * 
  * @param  {Array} fieldsRequired [the fields that you depend on]
  * @return {Generator}  this is the middleware that will get returned. 
  */
@@ -17,13 +18,22 @@ module.exports = function requireRequest(fieldsRequired) {
   return function* (next) {
     var requestbody = yield parse(this);
 
-    if(_.has(requestbody, fieldsRequired))  {
+    this.body = {
+      a: fieldsRequired,
+      b: requestbody
+    };
+
+    if(_.every(fieldsRequired, _.partial(_.has, requestbody)))  {
+    
       this.parsedRequest = requestbody;
       yield next;
+    
     } else {
+
       this.status = 400;
       var missingFieldsString = fieldsRequired.reduce((f1, f2) => f1 + '|' + f2);
-      var this.body = { message: `missing one of ${missingFieldsString}.` };
+      this.body = { message: `missing one of ${missingFieldsString}.` };
+    
     }
     
   };
