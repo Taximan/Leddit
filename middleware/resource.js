f32154100c7bd97b4c3cde6f8549e534ed0c7835
Dawid <dcz31249@gmail.com>
2015-10-22 11:59:50 +0200

    RESTful error handling

--- middleware/resource.js
+++ middleware/resource.js
@@ -36,8 +36,8 @@ exports.fetchOne = function(Model, related) {
       .fetch({ require: true, withRelated: related })
       .then(data => this.body = data.toJSON())
       .catch(err => {
-        console.log(err);
-        this.body = 'oopss..??';
+        this.status = 404;
+        this.body = { message: 'not found' };
       }); 
   };
 };
