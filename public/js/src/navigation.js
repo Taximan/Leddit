0422698e62132912c106e6a247b127d4f51d3b53
Dawid <dcz31249@gmail.com>
2015-10-24 09:23:04 +0200

    refactored

--- public/js/src/navigation.js
+++ public/js/src/navigation.js
@@ -1,27 +1,24 @@
 import '../../css/src/navigation.css';
 import $ from 'jquery';
 
-const navigation = (() => {
-  var $navEl = $('nav[role="navigation"]'),
-      $rightNavList = $navEl.find('ul.nav-right'),
-      $rightNavListItems = $rightNavList.find('li');
+var $navEl = $('nav[role="navigation"]'),
+    $rightNavList = $navEl.find('ul.nav-right'),
+    $rightNavListItems = $rightNavList.find('li');
 
-  function rightListItemClickEventHandler() {
-    if(!$(this).hasClass('active')) {
-      $rightNavListItems.removeClass('active');
-      $(this).addClass('active');
-    }
+function rightListItemClickEventHandler() {
+  if(!$(this).hasClass('active')) {
+    $rightNavListItems.removeClass('active');
+    $(this).addClass('active');
+  }
+}
+
+export default {
+  init() {
+    $rightNavListItems.on('click', rightListItemClickEventHandler);
+  },
+
+  destroy() {
+    $rightNavListItems.off('click');
   }
 
-  return {
-    init: function() {
-      $rightNavListItems.on('click', rightListItemClickEventHandler);
-    },
-    destroy: function() {
-      $rightNavListItems.off('click');
-    }
-  };
-
-}());
-
-export default navigation;
+};
